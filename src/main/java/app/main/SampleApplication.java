d59069c02d9f2b410007a4fa5f6f81edb17bcde0
Dave Syer <dsyer@pivotal.io>
2020-06-24 13:41:38 +0100

    Working sample

---
+++ src/main/java/app/main/SampleApplication.java
@@ -0,0 +1,44 @@
+package app.main;
+
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class SampleApplication {
+
+	@Bean
+	public CommandLineRunner runner(ConfigurableListableBeanFactory beans) {
+		return args -> {
+			System.err.println("Class count: " + ManagementFactory.getClassLoadingMXBean()
+					.getTotalLoadedClassCount());
+			System.err.println("Bean count: " + beans.getBeanDefinitionNames().length);
+			System.err.println(
+					"Bean names: " + Arrays.asList(beans.getBeanDefinitionNames()));
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+}
+
+@RestController
+class HomeController {
+
+	@Value("${app.value}")
+	private String value;
+
+	@GetMapping("/")
+	public String home() {
+		return value;
+	}
+}
